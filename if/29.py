0c577fdb8468b3122cf7377bff19b80e9b0acf70
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-11 19:35:30 +0500

    Create 29.py

---
+++ if/29.py
@@ -0,0 +1,11 @@
+x=int(input('x='))
+if x>0 and x%2==0:
+    print('musbat juft')
+if x>0 and x%2==1:
+    print('musbat toq')
+if x<0 and x%2==0:
+    print('manfiy juft')
+if x<0 and x%2==1:
+    print('manfiy toq')
+if x==0:
+    print('nolga teng')
