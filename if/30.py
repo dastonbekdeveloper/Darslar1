b17924d25a9d0b4abe99d7508703d2ee253895fb
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-11 19:35:59 +0500

    Create 30.py

---
+++ if/30.py
@@ -0,0 +1,15 @@
+x=int(input('x='))
+if x>=100 and x%2==0:
+    print('uch xonali juft')
+if x>=100 and x%2==1:
+    print('uch xonali toq')
+if 10<=x<100 and x%2==0:
+    print('ikki xonali juft')
+if 10<=x<100 and x%2==1:
+    print('ikki xonali toq')
+if 0<x<10 and x%2==0:
+    print('juft')
+if 0<x<10 and x%2==1:
+    print('toq')
+if x==0:
+    print('nolga teng')
