88b09db851b6a683633b555b0bbda94ed279555b
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-11 19:31:12 +0500

    Create 19.py

---
+++ if/19.py
@@ -0,0 +1,12 @@
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+d=int(input('d='))
+if a==b==c:
+    print(4)
+if a==b==d:
+    print(3)
+if a==c==d:
+    print(2)
+if b==c==d:
+    print(1)
