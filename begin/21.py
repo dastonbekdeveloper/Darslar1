c2c3438da061ea62ad98789ea94fdd47bb2271f4
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-14 20:17:20 +0500

    Create 21.py

---
+++ begin/21.py
@@ -0,0 +1,14 @@
+x1=int(input('x1='))
+y1=int(input('y1='))
+x2=int(input('x2='))
+y2=int(input('y2='))
+x3=int(input('x3='))
+y3=int(input('y3='))
+a=((x1-x2)**2+(y1-y2)**2)**0.5
+b=((x1-x3)**2+(y1-y3)**2)**0.5
+c=((x2-x3)**2+(y2-y3)**2)**0.5
+p=(a+b+c)/2
+S=(p*(p-a)*(p-b)*(p-c))**0.5
+P=p*2
+print(S)
+print(P)
