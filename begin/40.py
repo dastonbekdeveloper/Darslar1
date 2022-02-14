3c7befbfb7f4730e7764075e4f6f08f01d3722f8
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-14 20:25:24 +0500

    Create 40.py

---
+++ begin/40.py
@@ -0,0 +1,10 @@
+a1=int(input('a1='))
+b1=int(input('b1='))
+c1=int(input('c1='))
+a2=int(input('a2='))
+b2=int(input('b2='))
+c2=int(input('c2='))
+d=a1*b2-a2*b1
+x=(c1*b2-c2*b1)/d
+y=(a1*c2-a2*c1)/d
+print(x,y)
