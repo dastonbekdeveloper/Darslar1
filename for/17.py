e49706810fd096cd289c78562cccde858a75946b
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 17:26:39 +0500

    Create 17.py

---
+++ for/17.py
@@ -0,0 +1,12 @@
+a = float(input('a = '))
+n = int(input('n = '))
+kup = 1
+sum = 0
+if n>0 :       
+    for i in range(1,n+1):
+        kup*=a
+        print(a,'ning',i,'- darajasi',kup)
+        sum+=kup
+    print('Yig\'indisi:',sum)
+else:
+    print('n>0 bo\'lishi kerak')
