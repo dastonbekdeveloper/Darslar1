0379c8a75d1b96f72394c7f5f020001ec479a2ff
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 20:00:17 +0500

    Create milliardgacha_sonlarni_yozuv_bn_yozish.py

---
+++ for/milliardgacha_sonlarni_yozuv_bn_yozish.py
@@ -0,0 +1,191 @@
+a=int(input("a="))
+
+yuzmillionlik=a%1000000000//100000000
+onmillionlik=a%100000000//10000000      
+millionlik=a%10000000//1000000
+yuzminglik=a%1000000//100000
+onminglik=a%100000//10000
+minglik=a%10000//1000
+yuzlik=a%1000//100
+onlik=a%100//10
+birlik=a%10
+
+if yuzmillionlik==1:
+    print("biryuz",end=' ')
+if yuzmillionlik==2:
+    print("ikkiyuz",end=' ')
+if yuzmillionlik==3:
+    print("uchyuz",end=' ')
+if yuzmillionlik==4:
+    print("to`rtyuz",end=' ')
+if yuzmillionlik==5:
+    print("beshyuz",end=' ')    
+if yuzmillionlik==6:
+    print("oltiyuz",end=' ')   
+if yuzmillionlik==7:
+    print("yettiyuz",end=' ')   
+if yuzmillionlik==8:
+    print("sakkizyuz",end=' ')
+if yuzmillionlik==9:
+    print("to`qqizyuz",end=' ')    
+
+
+if onmillionlik==1:
+    print("o`n",end=' ')
+if onmillionlik==2:
+    print("yigirma",end=' ')
+if onmillionlik==3:
+    print("o`ttiz",end=' ')
+if onmillionlik==4:
+    print("qirq",end=' ')
+if onmillionlik==5:
+    print("ellik",end=' ')    
+if onmillionlik==6:
+    print("oltmish",end=' ')   
+if onmillionlik==7:
+    print("yetmish",end=' ')   
+if onmillionlik==8:
+    print("sakson",end=' ')
+if onmillionlik==9:
+    print("to`qson",end=' ')    
+
+
+if millionlik==1:
+    print("bir ",end='')
+if millionlik==2:
+    print("ikki ",end='')
+if millionlik==3:
+    print("uch ",end='')
+if millionlik==4:
+    print("to`rt ",end='')
+if millionlik==5:
+    print("besh ",end='')    
+if millionlik==6:
+    print("olti ",end='')   
+if millionlik==7:
+    print("yetti ",end='')   
+if millionlik==8:
+    print("sakkiz ",end='')
+if millionlik==9:
+    print("to`qqiz ",end='')    
+
+if millionlik > 0 or onmillionlik > 0 or yuzmillionlik>0:
+    print("million",end=' ')
+
+if yuzminglik==1:
+    print("biryuz",end=' ')
+if yuzminglik==2:
+    print("ikkiyuz",end=' ')
+if yuzminglik==3:
+    print("uchyuz",end=' ')
+if yuzminglik==4:
+    print("to`rtyuz",end=' ')
+if yuzminglik==5:
+    print("beshyuz",end=' ')    
+if yuzminglik==6:
+    print("oltiyuz",end=' ')   
+if yuzminglik==7:
+    print("yettiyuz",end=' ')   
+if yuzminglik==8:
+    print("sakkizyuz",end=' ')
+if yuzminglik==9:
+    print("to`qqizyuz",end=' ')    
+
+if onminglik==1:
+    print("o`n",end=' ')
+if onminglik==2:
+    print("yigirma",end=' ')
+if onminglik==3:
+    print("o`ttiz",end=' ')
+if onminglik==4:
+    print("qirq",end=' ')
+if onminglik==5:
+    print("ellik",end=' ')    
+if onminglik==6:
+    print("oltmish",end=' ')   
+if onminglik==7:
+    print("yetmish",end=' ')   
+if onminglik==8:
+    print("sakson",end=' ')
+if onminglik==9:
+    print("to`qson",end=' ')    
+
+if minglik==1:
+    print("bir",end='')
+if minglik==2:
+    print("ikki",end='')
+if minglik==3:
+    print("uch",end='')
+if minglik==4:
+    print("to`rt",end='')
+if minglik==5:
+    print("besh",end='')    
+if minglik==6:
+    print("olti",end='')   
+if minglik==7:
+    print("yetti",end='')   
+if minglik==8:
+    print("sakkiz",end='')
+if minglik==9:
+    print("to`qqiz",end='')   
+    
+if minglik > 0 or onminglik > 0 or yuzminglik>0:
+    print("ming",end=' ')
+        
+
+if yuzlik==1:
+    print("biryuz ",end='')
+if yuzlik==2:
+    print("ikkiyuz ",end='')
+if yuzlik==3:
+    print("uch yuz ",end='')
+if yuzlik==4:
+    print("to`rtyuz ",end='')
+if yuzlik==5:
+    print("beshyuz ",end='')    
+if yuzlik==6:
+    print("oltiyuz ",end='')   
+if yuzlik==7:
+    print("yettiyuz ",end='')   
+if yuzlik==8:
+    print("sakkizyuz ",end='')
+if yuzlik==9:
+    print("to`qqizyuz ",end='')    
+        
+if onlik==1:
+    print("o`n ")
+if onlik==2:
+    print("yigirma ")
+if onlik==3:
+    print("o`ttiz ")
+if onlik==4:
+    print("qirq ")  
+if onlik==5:
+    print("ellik ")
+if onlik==6:
+    print("oltmish ")
+if onlik==7:
+    print("yetmish ")
+if onlik==8:
+    print("sakson ")      
+if onlik==9:                                       
+    print("to`qson ",end='')
+
+if birlik==1:                                  
+    print("bir")
+if birlik==2:
+    print("ikki")
+if birlik==3:
+    print("uch")
+if birlik==4:
+    print("to`rt")
+if birlik==5:
+    print("besh")
+if birlik==6:
+    print("olti")
+if birlik==7:
+    print("yetti")
+if birlik==8:
+    print("sakkiz")
+if birlik==9:
+    print("to`qqiz")
