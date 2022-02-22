36f1bc5aa4e1c6d7c9f505fd70d85ca37672f8bd
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-22 20:17:30 +0500

    Create 37.py

---
+++ for/37.py
@@ -0,0 +1,10 @@
+n = int(n)
+
+summa = 0
+for i in range(1, n+1):
+  summa += i**i
+  print(i, ")  ", i, " ning ", i, " - darajasi ",i**i, " \n      Yig'indi: ", summa, " \n")
+
+print("Natija: = ", summa)
+
+print(" \n\n\n  by: Ruzmov_Javlonbek   ")
