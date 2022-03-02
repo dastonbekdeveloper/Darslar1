1bcaa5cf2b02869f6229352625d5f3523973cce9
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-03-02 22:07:34 +0500

    Create 28.py

---
+++ for2/28.py
@@ -0,0 +1,17 @@
+# for 28
+
+n = int(input('n = '))
+
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end='')
+    for j in range(2*i-1):
+        print('*',end='')
+    print()
+    
+for i in range(n-1,0,-1):
+    for j in range(n-i):
+        print(' ',end='')
+    for j in range(2*i-1):
+        print('*',end='')
+    print()
