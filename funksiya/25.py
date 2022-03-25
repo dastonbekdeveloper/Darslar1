62f0dfe5a0bdde0f0c1429501a16fad171cb9e90
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-03-25 09:26:15 +0500

    Create 25.py

---
+++ funksiya/25.py
@@ -0,0 +1,10 @@
+# def tekshir(a) :
+#     son = bool
+#     i = 1
+#     while a>i :
+#         if a/i==i :
+#             return i/i==1
+#             break
+#         i+=1
+# a = int(input())
+# print(tekshir(a))
