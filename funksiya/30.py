9508d9689bf6656b9a2518a03530236eaaf4bfdf
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-03-25 09:28:53 +0500

    Create 30.py

---
+++ funksiya/30.py
@@ -0,0 +1,12 @@
+# def raqam_aniqla(a,n):
+#     son = bool
+#     while a!=0 :
+#         if a%10==n :
+#             son = 0
+#             return n
+#         a//=10
+#     if son :
+#         return -1
+# a = int(input())
+# n = int(input())
+# print(raqam_aniqla(a,n))
